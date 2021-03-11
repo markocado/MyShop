6b2a71ef4d36016cab5550622db54d3073631a5f
markocado <37678120+markocado@users.noreply.github.com>
2021-03-11 10:11:24 +0800

    Save - Basket

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket: BaseEntity
+    {
+        // use virtual is useful as lazy loading when connected to database. 
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
